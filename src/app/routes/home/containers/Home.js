c4f0f669ffc71e17c3d32aac366bae3537bb54d0
Alice <liuyushu1988@gmail.com>
2017-05-16 00:24:05 +0800

    Initial Commit

---
+++ src/app/routes/home/containers/Home.js
@@ -0,0 +1,19 @@
+/**
+ * Created by griga on 11/30/15.
+ */
+
+import React from 'react'
+
+import WidgetGrid from '../../../components/widgets/WidgetGrid'
+import JarvisWidget  from '../../../components/widgets/JarvisWidget'
+
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div id="content">
+      <h1>这是个人首页</h1>
+      </div>
+    )
+  }
+}
